0f14a3f1596eeae30042ede3dd546455fb1c3ae4
karthik <karthikp@ece.ubc.ca>
2025-07-29 13:14:47 -0700

    Set error and delay flags to false

--- karthik/activities/lecture6/server/simpleserver.py
+++ karthik/activities/lecture6/server/simpleserver.py
@@ -12,10 +12,10 @@ PORT = 8080	# Port to connect to
 
 # Begin configuration - change these parameters for the server to have interesting behaviors
 
-delay = True		        # Should there be a delay ?
+delay = False		        # Should there be a delay ?
 minDelay = 0 		        # if so, the minimum delay in seconds
 maxDelay = 2 		        # if so, the maximum delay in seconds
-introduceError = True 	    # Should we have the server returning an error
+introduceError = False 	    # Should we have the server returning an error
 errorProb = 0.5	            # If so, with what probability should we introduce errors
 
 # End of configuration - change the code below only if you know what you're doing
