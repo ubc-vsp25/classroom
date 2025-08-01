e7a3790448f75890874f520d4630af02e3829e77
karthik <karthikp@ece.ubc.ca>
2025-08-01 13:12:28 -0700

    Added demos for lecture9 (not activities)

---
+++ karthik/activities/lecture9/xss-vulnerable-1.js
@@ -0,0 +1,36 @@
+// Warning: DO NOT RUN THIS in a public setting
+// it is an example to demonstrate a xss vulnerability
+
+// Mount an XSS attack by including a script as part of the URL
+// e.g., /search?text=<script>alert("ATTACKED!")</script>
+
+const express = require('express');	// need express to run this example - i.e. npm i express
+
+const PORT = 3000;
+
+let app = express();
+
+// need to disable XSS-Protection to mitigate XSS Auditor
+app.use((req, res, next)=> {
+  res.set('X-XSS-Protection', 0);
+  next();
+});
+
+app.get('/search', (req, res, next)=> {
+  console.log(req.query);
+  if (req.query.text){
+    // if req.query.text is something like <script>sendToBadServer(document.cookie)</script>
+    // i.e., http://example.com/search?text=<script>sendToBadServer(document.cookie)</script>
+    // and the attacker sends this link to a victim, then upon the victim accessing this page,
+    // the cookie on the victim's browser will be stolen by the attacker
+    res.status(200)
+      .send(req.query.text + " could not be found");
+  }
+  else res.status(200).send("Enter a query string to exploit this page. E.g., /search?text=Foo");
+});
+
+app.get('/', (req, res, next)=> {
+	res.status(200).send('Go to: <b>/search?text=Foo</b> to access the vulnerable page.<br/>Try: <b>/search?text=&lt;script&gt;alert("HAHA")&lt;/script&gt;</b>');
+});
+
+app.listen(PORT, ()=> console.log("server started\n-----\n"));
---
+++ karthik/activities/lecture9/xss-vulnerable-2.js
@@ -0,0 +1,102 @@
+// Warning: DO NOT RUN THIS in a public setting
+// it is an example to demonstrate a xss vulnerability
+// 
+// Type in something like <script>alert("You are attacked!")</script>
+// in the text input to mount a XSS attack.
+// The user input is not sanitized and it is rendered verbatim
+// in the HTML, resulting in executing the script entered by the user.
+// 
+// IMPORTANT: This program generates a temporary file named temp-bad.data
+// Remove the file after demonstration
+
+const fs = require('fs');
+const util = require('util');
+const express = require('express');	// need express to run this example - i.e. npm i express
+
+const PORT = 3000;
+const DATAFILE = './temp-bad.data';
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+let app = express();
+let data = {
+  comments: [{
+    username: 'Alice',
+    content: "Hey guys! Let's all hang out!"
+  },
+  {
+    username: 'Bob',
+    content: "Yeah! I don't know you, but let's hang!"
+  }]
+};
+
+// This server does not sanitize the content given by the user.
+// The content could be something like <script>steal(document.cookie)</script>
+// and this will be rendered by the web server as-is.
+// The attack will impact everyone who visits the website.
+function addComment(username, content){
+  data.comments.push({
+    username: username,
+    content: content
+  });
+  return writeFile(DATAFILE, JSON.stringify(data))
+}
+
+// asynchronously read a file and load data
+// if it doesn't exist create it
+readFile(DATAFILE)
+.then((raw)=> {
+  data = JSON.parse(raw);
+  return 'Loaded File'
+}, (err)=> (err.code === 'ENOENT' ? writeFile(DATAFILE, '{}').then(()=> 'Created File') : 'Unknown Error while loading file'))
+.then(console.log);
+
+app.use(express.urlencoded({ extended: true }));
+
+// need to disable XSS-Protection to mitigate XSS Auditor
+app.use((req, res, next)=> {
+  res.set('X-XSS-Protection', 0);
+  next();
+});
+
+app.get('/', (req, res, next)=> {
+  
+  // This web server simply renders the user-given content
+  // without sanitizing it. Any user with the ability to post
+  // to this page can inject malicious code, which would be
+  // executed in every visitor's browser
+  let comments = data.comments.map((item)=> `<div>
+  <span><b>${item.username}</b> says:</span>  ${item.content}
+</div>`).join('\n');
+
+  let form = `<form action="/add" method="POST">
+<div><input name="username" placeholder="Username"/></div>
+<textarea name="content" placeholder="Content"></textarea>
+<input type="submit" value="Submit"/></form>`;
+
+  let page = `<html>
+  <head>
+  </head>
+  <body>
+    <h1>Comments</h1>
+    ${comments}
+    <div>
+      ${form}
+    </div>
+  </body>
+</html>`;
+
+  res.status(200).send(page);
+});
+
+app.post('/add', (req, res, next)=> {
+  console.log(req.body);
+  if (req.body.username && req.body.content){
+    addComment(req.body.username, req.body.content)
+      .then(()=> res.redirect('/'));
+  }
+  else res.status(403).send('Fill in both username and content');
+});
+
+app.listen(PORT, ()=> console.log("server started\n-----\nRemember to remove temp-bad.data file after the demonstration"));
